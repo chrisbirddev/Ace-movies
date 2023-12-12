38e4b7bd20c2d4574180bf9f7778a93ce6f47b5f
Christopher <chris.bird@outlook.com.br>
2023-12-12 09:43:55 +0000

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App"><h1 className="h1">Hello</h1> </div>
+    </>
+  )
+}
+
+export default App
